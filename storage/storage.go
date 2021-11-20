8820cd1ae948b21330ae6e514872f0610a644016
rumikk <rumikk@gmail.com>
2021-11-20 15:27:00 +0300

    Update

--- storage/storage.go
+++ storage/storage.go
@@ -1,22 +1,25 @@
 package storage
 
 import (
-	"actgo/parser"
 	"encoding/binary"
 	bolt "go.etcd.io/bbolt"
 	"gopkg.in/yaml.v3"
 	"time"
 )
 
-type Storage struct{}
-
-type Entry struct {
-	CreatedAt int64          `yaml:"created_at"`
-	Process   parser.Process `yaml:"process"`
+type Storage struct {
+	Db *bolt.DB
 }
 
-func Init(db *bolt.DB) error {
-	return db.Update(func(tx *bolt.Tx) error {
+type Entry struct {
+	CreatedAt int64  `yaml:"created_at"`
+	Name      string `yaml:"name"`
+	Selected  string `yaml:"selected"`
+	Extracted string `yaml:"extracted"`
+}
+
+func (s *Storage) Init() error {
+	return s.Db.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists([]byte("entries"))
 		if err != nil {
 			return err
@@ -25,8 +28,11 @@ func Init(db *bolt.DB) error {
 	})
 }
 
-func (s *Storage) AddEntry(entry *Entry, db *bolt.DB) error {
-	return db.Update(func(tx *bolt.Tx) error {
+func (s *Storage) AddEntry(entry *Entry) error {
+	if s.FindEntry(entry.Name, entry.Extracted).Extracted != "" {
+		return nil
+	}
+	return s.Db.Update(func(tx *bolt.Tx) error {
 		entry.CreatedAt = time.Now().Unix()
 		b := tx.Bucket([]byte("entries"))
 		id, err := b.NextSequence()
@@ -39,6 +45,26 @@ func (s *Storage) AddEntry(entry *Entry, db *bolt.DB) error {
 		}
 		return b.Put(itob(id), entryYaml)
 	})
+}
+
+func (s *Storage) FindEntry(name string, extracted string) *Entry {
+	var entry Entry
+	s.Db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("entries"))
+		b.ForEach(func(k, v []byte) error {
+			err := yaml.Unmarshal(v, &entry)
+			if err != nil {
+				return err
+			}
+			if entry.Name == name && entry.Extracted == extracted {
+				return nil
+			}
+			entry = Entry{}
+			return nil
+		})
+		return nil
+	})
+	return &entry
 }
 
 func itob(v uint64) []byte {
