188bc1ed5cb4c0881014b4ff6566cc1061cf2c7c
rumikk <rumikk@gmail.com>
2021-11-21 23:05:13 +0300

    Update

--- actions/action_replace/replace.go
+++ actions/action_replace/replace.go
@@ -1,6 +1,7 @@
 package action_replace
 
 import (
+	"log"
 	"strings"
 )
 
@@ -8,6 +9,9 @@ type Action struct{}
 
 func (a *Action) Perform(input string, options string) string {
 	opt := strings.Split(options, "|")
+	if len(opt) < 2 {
+		log.Fatal("Error when processing input: " + input + " options: " + options)
+	}
 	output := strings.ReplaceAll(input, opt[0], opt[1])
 	return output
 }
