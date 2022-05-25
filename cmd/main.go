c968d5e989399549b1f20905f6813cd912d30f8e
rumikk <rumikk@gmail.com>
2022-05-25 22:11:26 +0300

    wip

--- cmd/main.go
+++ cmd/main.go
@@ -2,77 +2,33 @@ package main
 
 import (
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"github.com/rumikk/actgo/actions"
 	"github.com/rumikk/actgo/parser"
-	"github.com/rumikk/actgo/storage"
-	bolt "go.etcd.io/bbolt"
 	"log"
-	"net/http"
 	"os"
-	"strings"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: act [db] [config]")
+		fmt.Println("Usage: act [Actfile] [DB]")
 		os.Exit(1)
 	}
 
-	processFile, err := os.ReadFile(os.Args[2])
+	actFile, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	processes, err := parser.NewProcessParser(processFile)
+	jobs, err := parser.Parse(actFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	db, err := bolt.Open(os.Args[1], 0600, &bolt.Options{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	s := storage.Storage{Db: db}
-	s.Init()
-
-	for _, process := range processes {
-		page, err := GetPage(process.Url)
-		if err != nil {
-			log.Println(err)
-			continue
+	for _, job := range jobs {
+		var next string
+		for _, action := range job.Actions {
+			a := actions.NewAction(action.Name)
+			next = a.Perform(next, action.Options)
 		}
-		page.Find(strings.TrimSpace(process.Selector)).Each(func(i int, selection *goquery.Selection) {
-			input := selection.AttrOr("href", "")
-			if input != "" {
-				if s.FindEntry(input).Input == "" {
-					var output string
-					output = input
-					for _, action := range process.Actions {
-						newAction := actions.NewAction(action.Name)
-						output = newAction.Perform(output, action.Options)
-					}
-					s.AddEntry(&storage.Entry{
-						Name:   process.Name,
-						Input:  input,
-						Output: output,
-					})
-				}
-			}
-		})
 	}
-}
-
-func GetPage(url string) (*goquery.Document, error) {
-	res, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	return doc, nil
 }
